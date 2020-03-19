14ceb721b81aa968c9530c0cbe3352441344dbbc
joseluisgs <joseluisgonsan@gmail.com>
2020-03-19 11:28:26 +0100

    Initial commit

---
+++ src/main/java/com/joseluisgs/productosapirest/ProductosapirestApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisgs.productosapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosapirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseluisgs/productosapirest/ProductosapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisgs.productosapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
