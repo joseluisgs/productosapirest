97d0f6479704ce68fa896b564f35c336cc3b8921
joseluisgs <joseluisgonsan@gmail.com>
2020-03-19 13:03:11 +0100

    Controlador: ResponseEntity

--- src/main/java/com/joseluisgs/productosapirest/controladores/ProductoController.java
+++ src/main/java/com/joseluisgs/productosapirest/controladores/ProductoController.java
@@ -5,6 +5,8 @@ import com.joseluisgs.productosapirest.modelos.Producto;
 import com.joseluisgs.productosapirest.repositorios.ProductoRepositorio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,46 +27,64 @@ public class ProductoController {
 
     /**
      * Lista todos los productos
-     * @return Lista todos los productos
+     * @return 404 si no hay productos, 200 y lista de productos si hay uno o más
      */
     @GetMapping("/productos")
-    public List<Producto> obetenerTodos() {
+    public ResponseEntity<?> obetenerTodos() {
         // Devolvemos todos
-        return productoRepositorio.findAll();
+        //return productoRepositorio.findAll();
+
+        // Actualizamos con ResponseEntity
+        List<Producto> result = productoRepositorio.findAll();
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
     }
 
 
     /**
      * Obtiene un producto con un id específico
      * @param id id del producto
-     * @return Producto si lo encuetra o nulo
+     * @return 404 si no encuentra el producto, 200 y el producto si lo encuentra
      */
     @GetMapping("/productos/{id}")
-    public Producto obtenerProducto(@PathVariable Long id) {
+    public ResponseEntity<?> obtenerProducto(@PathVariable Long id) {
         // Devolvemos si existe
-        return productoRepositorio.findById(id).orElse(null);
+        //return productoRepositorio.findById(id).orElse(null);
+
+        Producto result = productoRepositorio.findById(id).orElse(null);
+        if (result == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(result);
     }
 
 
     /**
      * Insertamos un nuevo producto
      * @param nuevo nuevo producto a insertar
-     * @return producto insertado correctamente
+     * @return 201 y el producto insertado
      */
     @PostMapping("/productos")
-    public Producto nuevoProducto(@RequestBody Producto nuevo) {
+    public ResponseEntity<?> nuevoProducto(@RequestBody Producto nuevo) {
         // Salvamos
-        return productoRepositorio.save(nuevo);
+        //return productoRepositorio.save(nuevo);
+
+        Producto saved = productoRepositorio.save(nuevo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     /**
      * Editamos un producto
      * @param editar producto a editar
      * @param id id del producto a editar
-     * @return producto editar
+     * @return 200 Ok si la edición tiene éxito, 404 si no se encuentra el producto
      */
     @PutMapping("/productos/{id}")
-    public Producto editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+    public ResponseEntity<?> editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+        /*
         // Si existe
         if (productoRepositorio.existsById(id)) {
             // Le ponemos el id que teníamos
@@ -75,15 +95,28 @@ public class ProductoController {
             // Devolvemos null
             return null;
         }
+        */
+
+        // Utilizamos Stream de Java 8 con notacion funcional
+        // Podriamos haber cambiado el código anterior
+        return productoRepositorio.findById(id).map(p -> {
+            p.setNombre(editar.getNombre());
+            p.setPrecio(editar.getPrecio());
+            return ResponseEntity.ok(productoRepositorio.save(p));
+        }).orElseGet(() -> {
+            return ResponseEntity.notFound().build();
+        });
+
     }
 
     /**
      * Borra un producto con un id espcífico
      * @param id id del producto a borrar
-     * @return producto borrado si existe
+     * @return Código 204 sin contenido
      */
     @DeleteMapping("/productos/{id}")
-    public Producto borrarProducto(@PathVariable Long id) {
+    public ResponseEntity<?> borrarProducto(@PathVariable Long id) {
+        /*
         // Si existe
         if (productoRepositorio.existsById(id)) {
             // Lo encotramos
@@ -94,6 +127,12 @@ public class ProductoController {
         } else
             // devolvemos null
             return null;
+    }
+         */
+
+        // Se puede usar el código anterior incluyendo los RespnseEntity
+        productoRepositorio.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
